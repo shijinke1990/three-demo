c49eae8f54df2cfd98be5df8c4109ec5c820ebe3
shijinke1990 <shijinke1990@qq.com>
2024-07-24 06:59:47 +0800

    feat: Add Sass dependency and create DemoView component
    - Add Sass dependency to package.json
    - Create DemoView.vue component with canvas element
    - Initialize WebGL context and clear the canvas in DemoView.vue
    - Remove unnecessary code from App.vue and import DemoView component

---
+++ src/views/DemoView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <canvas id="canvas" width="400" height="400"></canvas>
+
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+onMounted(() => {
+    const ctx = document.getElementById('canvas')
+    const gl = ctx.getContext('webgl')
+    // const gl = document.getElementById('canvas').getContext('webgl')
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+    gl.clear(gl.COLOR_BUFFER_BIT)
+})
+
+
+</script>
+
+
+<style lang="scss" scoped>
+// #canvas {
+//     width: 100px;
+//     height: 100px;
+//     display: block;
+// }
+</style>
